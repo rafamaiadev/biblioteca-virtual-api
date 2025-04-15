02028b534d94e65b8aae6d25d1e739bac626935f
rafamaiadev <rafamaiadev@gmail.com>
2025-04-15 19:15:01 -0300

    primeiro commit

---
+++ src/main/java/br/com/raphael/biblioteca_virtual_api/BibliotecaVirtualApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.raphael.biblioteca_virtual_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaVirtualApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaVirtualApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/raphael/biblioteca_virtual_api/BibliotecaVirtualApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.raphael.biblioteca_virtual_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaVirtualApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
